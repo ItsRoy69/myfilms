fb00c2574b06ca342c4fea53183e6e4c079ab4c8
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-02-17 21:13:36 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from './Components/Login.js';
 import Footer from './Components/Footer.js';
@@ -9,15 +9,15 @@ import Detail from './Components/Detail.js';
 
 const App = () => {
     return (
-        <>
-            <Navbar />
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route exact path="/home" element={<Home />} />
-              <Route exact path="/detail/:id" element={<Detail />} />
-            </Routes>
-            <Footer />
-        </>
+      <div className='App' >
+        <Navbar />
+        <Routes>
+            <Route exact path="/" element={ <Login/>} ></Route>
+            <Route exact path="/home" element={ <Home/>} ></Route>
+            <Route exact path="/detail/:id" element={ <Detail/>} ></Route>
+        </Routes>
+        <Footer />
+      </div>
     )
 }
 
