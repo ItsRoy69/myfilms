749d38b3e0d07b6c83c915a5bc768193357172cc
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-02-26 12:15:00 +0530

    ...

--- client/src/Firebase.js
+++ client/src/Firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/compat/app';
-import "firebase/firestore"
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDkgRPlRbvWX_oR9bkHdMQxdZt6ThTjy5w",
